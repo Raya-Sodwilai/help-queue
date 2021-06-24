231c852bebcb5e9a9c180a8319997fe63e7eaab2
Raya Sodwilai <sodwilai@pdx.edu>
2021-06-24 11:16:04 -0700

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import TicketControl from "./TicketControl";
+// import TicketList from "./TicketList";
+
+function App(){
+  return ( 
+    <React.Fragment>
+      <Header />
+      <TicketControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
